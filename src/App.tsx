906ebdb779b777c8be27f5d9a81d61ff22f7edc5
sommygee <your-email@example.com>
2026-02-12 23:56:48 +0100

    new project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Task Dashboard</h1>
+      </header>
+
+      <main className="main">
+        <p>Let's start adding tasks soon...</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
